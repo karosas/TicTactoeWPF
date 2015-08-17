fd805e40005a890bab6eccdb273dc075c4538ca5
Edgaras Ausvicas <e.ausvicas@gmail.com>
2015-08-17 11:55:03 +0200

    Works, but needs refactoring, currently all logic is in single View class.

--- TicTactoeWPF/View/MainWindow.xaml.cs
+++ TicTactoeWPF/View/MainWindow.xaml.cs
@@ -20,9 +20,99 @@ namespace TicTactoeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private int turn;
+        private int i, j;
         public MainWindow()
         {
             InitializeComponent();
+            turn = 1;
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            turn = 1;
+        }
+
+        private void win(string btnContent)
+        {
+            if ((Btn1.Content == btnContent & Btn2.Content == btnContent &
+                 Btn3.Content == btnContent)
+               | (Btn1.Content == btnContent & Btn4.Content == btnContent &
+                 Btn7.Content == btnContent)
+               | (Btn1.Content == btnContent & Btn5.Content == btnContent &
+                 Btn9.Content == btnContent)
+               | (Btn2.Content == btnContent & Btn5.Content == btnContent &
+                 Btn8.Content == btnContent)
+               | (Btn3.Content == btnContent & Btn6.Content == btnContent &
+                 Btn9.Content == btnContent)
+               | (Btn4.Content == btnContent & Btn5.Content == btnContent &
+                 Btn6.Content == btnContent)
+               | (Btn7.Content == btnContent & Btn8.Content == btnContent &
+                 Btn9.Content == btnContent)
+               | (Btn3.Content == btnContent & Btn5.Content == btnContent &
+                 Btn7.Content == btnContent))
+            {
+                if (btnContent == "O")
+                {
+
+                    MessageBox.Show("PLAYER O WINS");
+                    //Label1.Content = ++i;
+                }
+                else if (btnContent == "X")
+                {
+                    MessageBox.Show("PLAYER X WINS");
+                    //Label2.Content = ++j;
+                }
+                disableButtons();
+            }
+            else
+            {
+                foreach (Button btn in BtnPanel.Children)
+                {
+                    if (btn.IsEnabled == true)
+                        return;
+                }
+                MessageBox.Show("GAME OVER NO ONE WINS");
+            }
+        }
+
+        private void disableButtons()
+        {
+            foreach(Button btn in BtnPanel.Children)
+            {
+                btn.IsEnabled = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if( turn == 1)
+            {
+                btn.Content = "O";
+                PlayerTurn.Text = "X";
+            }
+            else
+            {
+                btn.Content = "X";
+                PlayerTurn.Text = "O";
+            }
+            btn.IsEnabled = false;
+            win(btn.Content.ToString());
+            turn++;
+            if (turn > 2)
+                turn = 1;
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(Button btn in BtnPanel.Children)
+            {
+                btn.Content = "";
+                btn.IsEnabled = true;
+            }
+        } 
+    
     }
 }
